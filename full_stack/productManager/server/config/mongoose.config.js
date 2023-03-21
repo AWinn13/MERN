e69141c4331a17624a15a725256043208ef2d735
AWinn13 <anthonymichaelwinn@gmail.com>
2023-03-21 17:45:10 -0400

    update

--- full_stack/productManager/server/config/mongoose.config.js
+++ full_stack/productManager/server/config/mongoose.config.js
@@ -4,7 +4,7 @@ const dbName = process.env.DB;
 const username = process.env.DB_USERNAME;
 const pw = process.env.DB_PASSWORD;
 const DBURI = process.env.dbURI;
-const uri = `mongodb+srv://awinn:axilla!2@cluster0.w8jktar.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${username}:${pw}@${DBURI}?retryWrites=true&w=majority`;
 
 
 mongoose.connect(uri, {
